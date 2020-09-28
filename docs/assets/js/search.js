db923575d2016ae885bd6112c3b0abd27d725031
Maxym Kot <maximkott@gmail.com>
2020-09-28 16:25:16 +0200

    Add docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":4194304,"name":"ListInitializer","url":"globals.html#listinitializer","classes":"tsd-kind-type-alias tsd-has-type-parameter"},{"id":1,"kind":4194304,"name":"UseList","url":"globals.html#uselist","classes":"tsd-kind-type-alias"},{"id":2,"kind":65536,"name":"__type","url":"globals.html#uselist.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-has-type-parameter","parent":"UseList"},{"id":3,"kind":64,"name":"useList","url":"globals.html#uselist","classes":"tsd-kind-function tsd-has-type-parameter"}]};
